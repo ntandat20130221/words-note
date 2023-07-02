5ade55e3a8539e685c3dfe961bae3d8a3806a58b
ntandat20130221 <20130221@st.hcmuaf.edu.vn>
2023-07-02 21:48:02 +0700

    Add WordsFragment and SettingsFragment

---
+++ app/src/main/java/com/example/wordnotes/ui/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.wordnotes.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.wordnotes.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    navController.navigate(R.id.action_settingsFragment_to_wordsFragment)
+                    true
+                }
+
+                R.id.menu_settings -> {
+                    navController.navigate(R.id.action_wordsFragment_to_settingsFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
